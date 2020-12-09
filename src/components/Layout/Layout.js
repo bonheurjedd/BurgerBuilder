6f47959e8532562015978f363c13df77a51a6195
jed <impossibleisnothing@mail.com>
2020-12-08 23:02:56 -0500

    My burger App starting Point

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Layout.module.css';
+const Layout = (props) => {
+ return (
+  <>
+   <div>Toolbar, Sidedrawer, Backdrop</div>
+   <main className={classes.Content}>
+    {props.children}
+   </main>
+  </>
+ )
+}
+export default Layout;
